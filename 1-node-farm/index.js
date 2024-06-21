aac74a635c69ba39cffd3f2535ebac0e06aaed5f
PratikPrabhakarrao <Pratikprabhakarraoatwork@gmail.com>
2024-06-21 13:26:18 +0530

    commit 16

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,6 +3,10 @@ const http = require("http"); // gives us networkig capability
 const url = require("url");
 
 
+const replaceTemplate = require('./final/modules/replaceTemplate')
+
+ 
+
 //////////////////////////////////////////////////////////////////
 //// FILES
 
@@ -34,19 +38,7 @@ const url = require("url");
 // Server
 
 
-const replaceTemplate = (temp, product)=>{
-  let output =  temp.replace(/{%PRODUCTNAME%}/g,  product.productName);
-  output = output.replace(/{%IMAGE%}/g,  product.image);
-  output = output.replace(/{%PRICE%}/g,  product.price);
-  output = output.replace(/{%FROM%}/g,  product.from);
-  output = output.replace(/{%NUTRIENT%}/g,  product.nutrient);
-  output = output.replace(/{%QUANTITY%}/g,  product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g,  product.description);
-  output = output.replace(/{%ID%}/g,  product.id);
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,  'not-organic');
-  return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/final/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/final/templates/template-card.html`, 'utf-8');
